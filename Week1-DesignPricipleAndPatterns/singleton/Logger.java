e472f9c256cb982be2a38fc0b54301c951fa4a27
Thanuja2102 <thanuja21025@gmail.com>
2025-06-19 14:04:30 +0530

    Add files via upload
    First  Commit

---
+++ Week1-DesignPricipleAndPatterns/singleton/Logger.java
@@ -0,0 +1,18 @@
+package singleton;
+
+public class Logger {
+    private static final Logger instance = new Logger();
+
+    private Logger() {
+        System.out.println("Logger instance created.");
+    }
+
+    public static Logger getInstance() {
+        return instance;
+    }
+
+    public void log(String message) {
+        System.out.println("[LOG]: " + message);
+    }
+}
+
